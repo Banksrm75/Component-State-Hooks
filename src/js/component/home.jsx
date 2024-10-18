9edeaf110ca1cd5a91706e58d3019fc08610fc8d
Rebekah Banks <168665115+Banksrm75@users.noreply.github.com>
2024-10-18 23:25:13 +0000

    useState hook

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,60 @@
-import React from "react";
+// add "useState" to the import statement
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+	// create a useState hook:
+
+		 //variable  //setter function		//useState function call with initial value within the ()
+	const [counter, setCounter] = useState(0);
+	// Every time you call "setCounter" it will reassign the value
+
+
+	const [text, setText] = useState("hello");
+	const [like, setLike] = useState(true);
+
+	// Set the "onClick" function to   '  onClick = {() => {}}          ''
+			// call the function (  "setCounter()"  ) and put what the new value 
+			//		SHOULD BE inside the ()
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="CounterAndButton">
+				<h1>{counter}</h1>
+				<button onClick={() => {
+					setCounter(counter + 1)
+				}}>+1</button>
+
+				<button onClick={() => {
+					setCounter(counter - 1)
+				}}>-1</button>
+			</div>
+
+			<div className="ChangeText">
+				<h1>{text}</h1>
+				<button onClick={() => {
+					setText("Goodbye")
+				}}>Change to Goodbye</button>
+			</div>
+
+
+			<div className="checkbox">
+				<label>
+					<input 
+					type="checkbox"
+					checked={like}
+					onChange={(e) => {
+						setLike(e.target.checked)
+					}}
+					/>
+					I liked this
+				</label>
+				<p>You {like ? 'liked' : 'did not like'} this.</p>
+
+			</div>
+			
+
+		</>
+
 	);
 };
 
